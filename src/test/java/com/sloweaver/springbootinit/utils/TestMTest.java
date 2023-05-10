25ad275a29fa763b34bdfa4afd4e98523fdb2865
sloweaver <zuan64330509>
2023-05-10 10:19:08 +0800

    后台springboot 添加用户功能

---
+++ src/test/java/com/sloweaver/springbootinit/utils/TestMTest.java
@@ -0,0 +1,29 @@
+package com.sloweaver.springbootinit.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestMTest {
+
+    @Test
+    void cretaemap() {
+
+    }
+
+    @Test
+    void copyMap() {
+
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void hello(){
+        TestM a = new TestM();
+        a.hello();
+
+    }
+}
